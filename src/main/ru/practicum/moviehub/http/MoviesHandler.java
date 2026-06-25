b9e660cafdd5132f73a6d21bc8fc1ee738c4d528
PepeTheFrog13 <kolchev.m.a@yandex.ru>
2026-06-26 02:36:38 +0300

    Исправлен стиль

--- src/main/ru/practicum/moviehub/http/MoviesHandler.java
+++ src/main/ru/practicum/moviehub/http/MoviesHandler.java
@@ -45,92 +45,96 @@ public class MoviesHandler extends BaseHttpHandler {
             params = exchange.getRequestURI().getQuery().split("&");
         }
         //Обработка все GET эднпоинтов
-        if (method.equals("GET")) {
-            if (uri.length == 2 && params.length == 0) {
-                List<Movie> movieList = moviesStore.findAll();
-                String json = gson.toJson(movieList);
-                sendJson(exchange, 200, json);
-            }
-            if (uri.length == 2 && params.length == 1) {
-                String[] entry = params[0].split("=");
-                if (entry[0].equals("year")) {
-                    int year;
-                    try {
-                        year = Integer.parseInt(entry[1]);
-                        List<Movie> movieList = moviesStore.findByYear(year);
-                        String json = gson.toJson(movieList);
-                        sendJson(exchange, 200, json);
-                    } catch (Exception e) {
+        switch (method) {
+            case "GET" -> {
+                if (uri.length == 2 && params.length == 0) {
+                    List<Movie> movieList = moviesStore.findAll();
+                    String json = gson.toJson(movieList);
+                    sendJson(exchange, 200, json);
+                }
+                if (uri.length == 2 && params.length == 1) {
+                    String[] entry = params[0].split("=");
+                    if (entry[0].equals("year")) {
+                        int year;
+                        try {
+                            year = Integer.parseInt(entry[1]);
+                            List<Movie> movieList = moviesStore.findByYear(year);
+                            String json = gson.toJson(movieList);
+                            sendJson(exchange, 200, json);
+                        } catch (Exception e) {
+                            sendErrorResponse(exchange, "Некорректный параметр года", List.of("Не указан параметр year", "Указан неверный формат запроса"), 400);
+                            return;
+                        }
+                    } else {
                         sendErrorResponse(exchange, "Некорректный параметр года", List.of("Не указан параметр year", "Указан неверный формат запроса"), 400);
                         return;
                     }
+                }
+                if (uri.length == 3) {
+                    try {
+                        long id = Long.parseLong(uri[2]);
+                        Optional<Movie> movieOptional = moviesStore.findById(id);
+                        if (movieOptional.isEmpty()) {
+                            sendErrorResponse(exchange, "Фильм не найден", List.of("Указан несуществующий идентификатор!"), 404);
+                        } else {
+                            String json = gson.toJson(movieOptional.get());
+                            sendJson(exchange, 200, json);
+                        }
+                    } catch (Exception e) {
+                        sendErrorResponse(exchange, "Некорректный ID", List.of("Формат данных не соотвествует формату данных id"), 400);
+                    }
+                }
+            }
+            //Обработка POST эндпоинта
+            case "POST" -> {
+                if (!headers.containsKey("Content-Type")) {
+                    sendErrorResponse(exchange, "Отсутсвует нужный заголовок ", List.of("В заголовках запроса не указан Content-Type"), 415);
+                    return;
                 } else {
-                    sendErrorResponse(exchange, "Некорректный параметр года", List.of("Не указан параметр year", "Указан неверный формат запроса"), 400);
-                    return;
-                }
-            }
-            if (uri.length == 3) {
-                try {
-                    long id = Long.parseLong(uri[2]);
-                    Optional<Movie> movieOptional = moviesStore.findById(id);
-                    if (movieOptional.isEmpty()) {
-                        sendErrorResponse(exchange, "Фильм не найден", List.of("Указан несуществующий идентификатор!"), 404);
-                    } else {
-                        String json = gson.toJson(movieOptional.get());
-                        sendJson(exchange, 200, json);
+                    List<String> contentTypeValues = headers.get("Content-Type");
+                    if (contentTypeValues.isEmpty()
+                            || !contentTypeValues.contains("application/json")
+                            || !contentTypeValues.contains("charset=UTF-8")) {
+                        sendErrorResponse(exchange, "Неправильное значение заголовка Content-Type", List.of("Не указано значение application/json", "charset=UTF-8"), 415);
+                        return;
                     }
-                } catch (Exception e) {
-                    sendErrorResponse(exchange, "Некорректный ID", List.of("Формат данных не соотвествует формату данных id"), 400);
                 }
-            }
-        }
-        //Обработка POST эндпоинта
-        else if (method.equals("POST")) {
-            if (!headers.containsKey("Content-Type")) {
-                sendErrorResponse(exchange, "Отсутсвует нужный заголовок ", List.of("В заголовках запроса не указан Content-Type"), 415);
-                return;
-            } else {
-                List<String> contentTypeValues = headers.get("Content-Type");
-                if (contentTypeValues.isEmpty()
-                        || !contentTypeValues.contains("application/json")
-                        || !contentTypeValues.contains("charset=UTF-8")) {
-                    sendErrorResponse(exchange, "Неправильное значение заголовка Content-Type", List.of("Не указано значение application/json", "charset=UTF-8"), 415);
-                    return;
-                }
-            }
-            try {
-                JsonObject jsonObject = JsonParser.parseString(body).getAsJsonObject();
-                int year = jsonObject.get("year").getAsInt();
-                String title = jsonObject.get("title").getAsString();
-                int validateYear = LocalDate.now().getYear() + 1;
-                if (title.isEmpty() || title.length() > 100 || year < 1888 || year > validateYear) {
-                    sendErrorResponse(exchange, "Ошибка валидации", List.of("Название не должно быть пустым", "Год должен быть между 1888 и 2026"), 400);
-                    return;
-                }
-                Movie movie = moviesStore.addMovie(title, year);
-                sendJson(exchange, 201, gson.toJson(movie));
-            } catch (Exception e) {
-                sendErrorResponse(exchange, "Ошика десериализации", List.of("В теле запроса не указан неверный набор полей"), 400);
-            }
-        } else if (method.equals("DELETE")) {
-            if (uri.length == 3) {
                 try {
-                    long id = Long.parseLong(uri[2]);
-                    Optional<Movie> movieOptional = moviesStore.findById(id);
-                    if (movieOptional.isEmpty()) {
-                        sendErrorResponse(exchange, "Фильм не найден", List.of("Указан несуществующий идентификатор!"), 404);
-                    } else {
-                        moviesStore.deleteById(id);
-                        exchange.sendResponseHeaders(204, -1);
-                        exchange.close();
+                    JsonObject jsonObject = JsonParser.parseString(body).getAsJsonObject();
+                    int year = jsonObject.get("year").getAsInt();
+                    String title = jsonObject.get("title").getAsString();
+                    int validateYear = LocalDate.now().getYear() + 1;
+                    if (title.isEmpty() || title.length() > 100 || year < 1888 || year > validateYear) {
+                        sendErrorResponse(exchange, "Ошибка валидации", List.of("Название не должно быть пустым", "Год должен быть между 1888 и 2026"), 400);
+                        return;
                     }
+                    Movie movie = moviesStore.addMovie(title, year);
+                    sendJson(exchange, 201, gson.toJson(movie));
                 } catch (Exception e) {
-                    sendErrorResponse(exchange, "Некорректный ID", List.of("Формат данных не соотвествует формату данных id"), 400);
+                    sendErrorResponse(exchange, "Ошика десериализации", List.of("В теле запроса не указан неверный набор полей"), 400);
                 }
             }
-        } else {
-            exchange.sendResponseHeaders(405, -1);
-            exchange.close();
+            case "DELETE" -> {
+                if (uri.length == 3) {
+                    try {
+                        long id = Long.parseLong(uri[2]);
+                        Optional<Movie> movieOptional = moviesStore.findById(id);
+                        if (movieOptional.isEmpty()) {
+                            sendErrorResponse(exchange, "Фильм не найден", List.of("Указан несуществующий идентификатор!"), 404);
+                        } else {
+                            moviesStore.deleteById(id);
+                            exchange.sendResponseHeaders(204, -1);
+                            exchange.close();
+                        }
+                    } catch (Exception e) {
+                        sendErrorResponse(exchange, "Некорректный ID", List.of("Формат данных не соотвествует формату данных id"), 400);
+                    }
+                }
+            }
+            default -> {
+                exchange.sendResponseHeaders(405, -1);
+                exchange.close();
+            }
         }
     }
 }
